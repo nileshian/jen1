34a70851a59df4ba59110fa1d25fe8258d4d743e
nileshian <nileshian@gmail.com>
2022-07-20 14:33:37 +0530

    CommitBy Nilesh

---
+++ jenkinTest/src/test/java/Smoke/First.java
@@ -0,0 +1,21 @@
+package Smoke;
+
+import org.testng.annotations.Test;
+
+public class First {
+	@Test
+	public void TC001()
+	{
+		System.out.println("Inside Test1");
+	}
+	@Test
+	public void TC002()
+	{
+		System.out.println("Inside Test2");
+	}
+	@Test
+	public void TC003()
+	{
+		System.out.println("Inside Test3");
+	}	
+}
